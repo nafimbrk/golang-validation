782275b5c0fd739449f280fa733dc7cb9eb1129e
nafimbrk <nafimubarok2005@gmail.com>
2025-08-20 19:57:18 -0700

    validasi nested struct

---
+++ validation_test.go
@@ -0,0 +1,145 @@
+package golangvalidation
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func TestValidation(t *testing.T) {
+	var validate *validator.Validate = validator.New()
+	if validate == nil {
+		t.Error("validate is nil")
+	}
+}
+
+func TestValidationVariable(t *testing.T) {
+	validate := validator.New()
+
+	user := "eko"
+
+	err := validate.Var(user, "required")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func TestValidateTwoVariables(t *testing.T) {
+	validate := validator.New()
+
+	password := "rahasia"
+	confirmPassword := "rahasia"
+
+	err := validate.VarWithValue(password, confirmPassword, "eqfield")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func TestMultipleTag(t *testing.T) {
+	validate := validator.New()
+	user := "34423"
+
+	err := validate.Var(user, "required,numeric")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func TestTagParameter(t *testing.T) {
+	validate := validator.New()
+	user := "99454534566"
+
+	err := validate.Var(user, "required,numeric,min=5,max=10")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func TestStruct(t *testing.T) {
+	type LoginRequest struct {
+		Username string `validate:"required,email"`
+		Password string `validate:"required,min=5"`
+	}
+
+	validate := validator.New()
+	loginRequest := LoginRequest{
+		Username: "eko@example.com",
+		Password: "eko12346",
+	}
+
+	err := validate.Struct(loginRequest)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func TestValidationErrors(t *testing.T) {
+	type LoginRequest struct {
+		Username string `validate:"required,email"`
+		Password string `validate:"required,min=5"`
+	}
+
+	validate := validator.New()
+	loginRequest := LoginRequest{
+		Username: "eko",
+		Password: "eko",
+	}
+
+	err := validate.Struct(loginRequest)
+	if err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+		for _, fieldError := range validationErrors {
+			fmt.Println("error", fieldError.Field(), "on tag", fieldError.Tag(), "with error", fieldError.Error())
+		}
+	}
+}
+
+func TestStructCrossField(t *testing.T) {
+	type RegisterUser struct {
+		Username        string `validate:"required,email"`
+		Password        string `validate:"required,min=5"`
+		ConfirmPassword string `validate:"required,min=5,eqfield=Password"`
+	}
+
+	validate := validator.New()
+	request := RegisterUser{
+		Username:        "eko@example.com",
+		Password:        "eko12346",
+		ConfirmPassword: "eko12346",
+	}
+
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func TestNestedStruct(t *testing.T) {
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
+
+	type User struct {
+		Id      string  `validate:"required"`
+		Name    string  `validate:"required"`
+		Address Address `validate:"required"`
+	}
+
+	validate := validator.New()
+	request := User{
+		Id:   "",
+		Name: "",
+		Address: Address{
+			City:    "",
+			Country: "",
+		},
+	}
+
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
